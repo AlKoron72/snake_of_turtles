1221b68dd29e87f5b87c2b4d69a3d1bd78303879
Alexander Guenther <alexander.guenther@iee-extern.fraunhofer.de>
2025-10-02 14:53:40 +0200

    location is marked with an X

--- locator/main.py
+++ locator/main.py
@@ -2,14 +2,25 @@ import time
 from turtle import Turtle, Screen
 from typing import Tuple
 
-from locator.data_handler import DataHandler
+from data_handler import DataHandler
 
 game_on = True
-IMAGE_PATH = "blank_states_img.gif"
+IMAGE_PATH = r"locator\blank_states_img.gif"
+TIME = 51
 
 states_data = DataHandler("50_states.csv")
 
-def check_for(pos: list,) -> int:
+def move_input_to(text: str, correct:bool = True):
+    print(f"moving it ({text}) to: {correct}")
+
+def check_for_str(check:str) -> None:
+    low_check = check.lower()
+    if low_check in states_data.data.state:
+        print(f"JA!!! {low_check}")
+        
+
+
+def check_for(pos: list) -> int:
     x = states_data.data.x
     y = states_data.data.y
     state = states_data.data.state
@@ -25,14 +36,23 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("white")
 screen.title("Locator-Game")
-screen.addshape(IMAGE_PATH) # load image for turtle later
-antwort = screen.textinput("Antwort", "Antwort")
-#screen.tracer(0)
-#screen.listen()
 
+screen.addshape(IMAGE_PATH) # load image for turtle later
 img_turtle = Turtle()
 img_turtle.shape(IMAGE_PATH) # uses loaded image from screen.addshape
 pos_clicked = img_turtle.onclick(get_mouse_pos)
+
+looper = True
+timer = 0
+while looper:
+    antwort = screen.textinput(f"Antwort {timer+1}", "Antwort")
+    print(antwort.lower())
+    check_for_str(antwort)
+    timer += 1
+    
+    if timer > TIME:
+        looper = False
+    
 screen.listen()
 
 screen.mainloop()
